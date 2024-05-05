c91da4187b2eaaa64ede236493c346bfb1838ed8
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-05-05 15:01:22 -0500

    Added user service, custom exceptions, global exception handler, user patch dto

---
+++ src/main/java/com/example/usersapi/exception/DatesOrderException.java
@@ -0,0 +1,7 @@
+package com.example.usersapi.exception;
+
+public class DatesOrderException extends RuntimeException {
+    public DatesOrderException(String message) {
+        super(message);
+    }
+}
